8e83cec2711021b000147f8bb4bb4547494f7363
Shivansh Singh <2310shivansh@gmail.com>
2026-08-27 01:59:26 +0530

    chore(release): bump version to v1.2.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -11,7 +11,7 @@ import (
 // Build-time variables injected via -ldflags.
 var (
 	// Version is the semantic release version of Fabric.
-	Version = "1.2.0"
+	Version = "1.2.1"
 
 	// GitCommit is the Git commit SHA at build time.
 	GitCommit = "unknown"
